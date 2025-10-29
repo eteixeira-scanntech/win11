9fc855ddb6ee46688ce79e4f9426be60fe89f652
eteixeira-scanntech <eteixeira.scanntech@gmail.com>
2025-10-29 17:25:49 -0300

    Update skip.cmd

--- skip.cmd
+++ skip.cmd
@@ -1,2 +1,21 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/eteixeira-scanntech/win11/refs/heads/main/unattend.xml
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot	
+@echo off
+setlocal
+set URL=https://raw.githubusercontent.com/eteixeira-scanntech/win11/refs/heads/main/unattend.xml
+set DEST=C:\Windows\Panther\unattend.xml
+
+echo Creando carpeta Panther si no existe...
+mkdir "C:\Windows\Panther" >nul 2>&1
+
+echo Descargando archivo unattend.xml...
+curl -L "%URL%" -o "%DEST%"
+if not exist "%DEST%" (
+    echo Error: No se pudo descargar unattend.xml
+    pause
+    exit /b 1
+)
+
+echo Ejecutando Sysprep...
+cd /d %SystemRoot%\System32\Sysprep
+Sysprep.exe /oobe /quiet /reboot /unattend:%DEST%
+
+endlocal
